a85e579bd752b10bd13b04cd18031db3b68d3c3c
Tushar-A-Kumar <for.games.only.95347995@gmail.com>
2026-08-11 20:45:42 +0000

    Changed input to take files from terminal

--- src/source.cpp
+++ src/source.cpp
@@ -1,13 +1,29 @@
+#include <fstream>
 #include <iostream>
 #include "Lexer.h"
+#include "Parser.h"
 
-int main(){
-    std::string s;
-    getline(std::cin,s);
+int main(int argc , char* argv[]){
+    if(argc<2){
+        std::cerr<<"Usage: "<<argv[0]<<"<filename>\n";
+        return 1;
+    }
+
+    std::ifstream file(argv[1]);
+    if(!file.is_open()){
+        std::cerr<<"Error: Couldn't open the file\n";
+        return 1;
+    }
+
+    std::string s ,line;
+    while(std::getline(file,line)){
+        s+=line;
+    }
+
     Lexer lexer(s);
     auto tokens = lexer.tokenize();
 
-    for(auto i:tokens){
-        std::cout<<i.lexeme<<" "<<"\n";
-    }
+    Parser parser(tokens);
+    parser.parse();
+    return 0;
 }
